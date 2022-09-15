79986cfae94b88312d6201b1882f9c22b8868fff
TairineEllen <tairine.ellen@gmail.com>
2022-09-15 13:52:03 -0300

    create Create Ad Banner component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import './styles/main.css';
 import logo from './assets/logo.svg';
 import { MagnifyingGlassPlus } from 'phosphor-react'
 import { GameBanner } from './components/GameBanner';
+import { CreateAdBanner } from './components/CreateAdBanner';
 
 function App() {
   return (
@@ -17,32 +18,10 @@ function App() {
         <GameBanner banner='/game-3.png' title='Counter Strike' adsCount={5} />
         <GameBanner banner='/game-4.png' title='Apex Legends' adsCount={5} />
         <GameBanner banner='/game-5.png' title='Fortnite' adsCount={5} />
-        <GameBanner banner='/game-6.png' title='World of Warcraft' adsCount={5} />          
-       
+        <GameBanner banner='/game-6.png' title='World of Warcraft' adsCount={5} />       
       </div>
 
-      <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden'>
-        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
-          <div>
-            <strong className='text-2xl text-white font-black block'>
-              Não encontrou seu duo?
-            </strong>
-            <span className='text-zinc-400 block'>
-              Publique um anúncio para encontrar novos players!
-            </span>
-          </div>
-
-          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3'>
-            <MagnifyingGlassPlus size={24}/>
-            Publicar anúncio
-          </button>
-
-
-        </div>
-      </div>
-
-
-
+      <CreateAdBanner />
     </div>
   )
 }
