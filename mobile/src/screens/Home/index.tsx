d78d9de9a2797798b9e18ced5034549455f2109f
TairineEllen <tairine.ellen@gmail.com>
2022-09-16 15:05:01 -0300

    add useState

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,18 +1,17 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { View, Image, FlatList, Alert } from 'react-native';
 import { styles } from './styles';
 import logo from '../../assets/logo-nlw-esports.png';
 import { Heading } from '../../components/Heading';
-import { GAMES } from '../../utils/games';
-import { GameCard } from '../../components/GameCard';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
   useEffect(() => {
     fetch('https://d2b0-2804-14d-5480-8b4d-fc7a-9110-9bd8-7ced.sa.ngrok.io/games')
     .then(res => res.json())
-    .then(data => console.log('Entrou', data))
-    .catch(error => console.log('Aqui', error))
-  }, []) 
+    .then(data => setGames(data))
+    }, []) 
   return (
     <View style={styles.container}>
 
@@ -21,7 +20,7 @@ export function Home() {
       <Heading title='Encontre seu duo!' subtitle='Selecione o game que desejar jogar...' />
 
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={item => item.id} 
         renderItem={({ item }) => (
           <GameCard data={item} />
