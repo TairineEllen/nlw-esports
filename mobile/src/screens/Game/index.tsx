d460dfc4d6947d5fcb96292120bf61efb4bf7b83
TairineEllen <tairine.ellen@gmail.com>
2022-09-16 15:59:02 -0300

    add function to back to home

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,17 +1,35 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Background } from '../../components/Background';
-import { useRoute } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { styles } from './styles';
 import { GameParams } from '../../@types/navigation';
-
+import { TouchableOpacity, View, Image } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+import { THEME } from '../../theme';
+import logo from '../../assets/logo-nlw-esports.png';
+import { Heading } from '../../components/Heading';
 
 export function Game() {
   const route = useRoute();
   const game = route.params as GameParams;
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
 
   return (
     <Background>
       <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={handleGoBack}>
+            <Entypo name='chevron-thin-left' color={THEME.COLORS.CAPTION_300} size={20}/>
+          </TouchableOpacity>
+          <Image source={logo} style={styles.logo} />
+          <View style={styles.right}/>
+        </View>
+
+        <Heading title={game.title} subtitle='Conecte-se e comece a jogar!'/>
 
       </SafeAreaView>
     </Background>
