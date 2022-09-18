9cc125ec70892a62035b3928169fcc030643a8d4
TairineEllen <tairine.ellen@gmail.com>
2022-09-18 15:57:19 -0300

    add notifications config

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,8 +3,7 @@ import { Background } from './src/components/Background';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter';
 import { Loading } from './src/components/Loading'
 import { Routes } from './src/routes';
-
-
+import './src/services/notificationConfig';
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({
